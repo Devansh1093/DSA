449e0aaf7f93f61f4d3697d5cb08085a03c6051f
Devansh Roy <dr30031102@gmail.com>
2026-08-07 23:08:54 +0530

    Two pointer problem

---
+++ Problem23.java
@@ -0,0 +1,30 @@
+// Valid Palindrome 
+
+
+// M1  - Two pointer method 
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left =0;
+        int right = s.length()-1;
+
+        while(left<right){
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            while(left<right & !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+            if (Character.toLowerCase(s.charAt(left))
+                    != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+        }
+    }
+
