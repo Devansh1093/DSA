4019d0f3b53c0a06ece29b821b0d4da656d5c6da
Devansh Roy <dr30031102@gmail.com>
2026-08-21 19:40:02 +0530

    ReorderList

---
+++ Problem40.java
@@ -0,0 +1,47 @@
+// Reorder List
+
+
+class Solution {
+    public void reorderList(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        prev = slow;
+        slow = slow.next;
+        prev.next = null;
+
+        // Reverse second half
+        ListNode curr = slow;
+        prev = null;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // Merge
+        ListNode pointer = head;
+        ListNode point = prev;
+
+        while (point != null) {
+            ListNode temp1 = pointer.next;
+            ListNode temp2 = point.next;
+
+            pointer.next = point;
+            point.next = temp1;
+
+            pointer = temp1;
+            point = temp2;
+        }
+    }
+}
