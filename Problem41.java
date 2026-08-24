de60a10360cb47b5fa871a6c7b452b34ebab6209
Devansh Roy <dr30031102@gmail.com>
2026-08-24 23:09:35 +0530

    Stack

---
+++ Problem41.java
@@ -0,0 +1,38 @@
+// Stack
+
+
+// Baseball Game
+
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack <Integer> stack =  new Stack<>();
+        
+        for(int i =0;i<operations.length;i++){
+            
+           if(operations[i].equals("+") ){
+             int top = stack.pop();
+             int second = stack.peek();
+
+             stack.push(top);
+             stack.push(top+second);
+           }
+          else if(operations[i].equals("D")){
+            stack.push(2*stack.peek());
+           }
+          else if(operations[i].equals("C")){
+            stack.pop();
+           }
+           else{
+            stack.push(Integer.parseInt(operations[i]));
+           }
+        }
+                int sum = 0;
+
+        while (!stack.isEmpty()) {
+            sum += stack.pop();
+        }
+
+        return sum;
+
+    }
+}
