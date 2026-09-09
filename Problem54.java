4348d20937f7fef12c36ad6541716bddf185cbd8
Devansh Roy <dr30031102@gmail.com>
2026-09-09 18:21:04 +0530

    Binary Search

---
+++ Problem54.java
@@ -0,0 +1,28 @@
+// Serch Insert position
+
+
+
+
+public class Problem54 {
+    
+}
+
+
+class Solution {
+    public int searchInsert(int[] arr, int target) {
+     int left =0;
+     int right = arr.length;
+    
+     while(left <right){
+         int mid = left +(right-left)/2;
+          if (arr[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        
+    
+     }
+   return right ;
+    }
+}
